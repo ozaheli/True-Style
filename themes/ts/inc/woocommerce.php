3b7eb6f35bf5cc62f35d68d10f987214e9681440
Heli Oza <heli.oza@dcmail.ca>
2022-02-22 18:17:53 -0500

    changes in woocommerce.php

--- themes/ts/inc/woocommerce.php
+++ themes/ts/inc/woocommerce.php
@@ -10,6 +10,7 @@ function ts_use_block_editor_for_post_type($use_block_editor,$post_type ){
     if ('product' === $post_type) {
         $use_block_editor = true;
     }
+    return $use_block_editor;
 
 }
-add_filters( 'use_block_editor_for_post_type','ts_use_block_editor_for_post_type');
+add_filter( 'use_block_editor_for_post_type','ts_use_block_editor_for_post_type',10 ,2);
