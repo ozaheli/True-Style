3abf11dd3cb0970c2a5fd874962a2420a93e5e61
Heli Oza <heli.oza@dcmail.ca>
2022-03-06 13:43:48 -0500

    Made changes in woocommerce.php

--- themes/ts/inc/woocommerce.php
+++ themes/ts/inc/woocommerce.php
@@ -16,4 +16,11 @@ function ts_use_block_editor_for_post_type($use_block_editor,$post_type ){
 add_filter( 'use_block_editor_for_post_type','ts_use_block_editor_for_post_type',10 ,2);
 
 //add_filter('woocommerce_enqueue_style', '__return_false');
-remove_action('woocimmerce_after_single_product_summary','woocommerce_output_product_data_tabs',10);
+
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 4);
+
+function ts_woocommerce_single_product_summary(){
+    echo '<p style="color:#ff5252"><b>You\'ll love our products!</b></p>';
+}
+add_action('woocommerce_single_product_summary','ts_woocommerce_single_product_summary',6);
+
