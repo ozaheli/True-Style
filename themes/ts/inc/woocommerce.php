fb7f70b3ecacb3b4e3de88986353f868c09c4dc4
Heli Oza <heli.oza@dcmail.ca>
2022-03-21 12:09:43 -0400

    Making custom posts file

--- themes/ts/inc/woocommerce.php
+++ themes/ts/inc/woocommerce.php
@@ -16,9 +16,9 @@ function ts_use_block_editor_for_post_type($use_block_editor,$post_type ){
 }
 add_filter( 'use_block_editor_for_post_type','ts_use_block_editor_for_post_type',10 ,2);
 
-//add_filter('woocommerce_enqueue_style', '__return_false');
+add_filter('woocommerce_enqueue_style', '__return_false');
 
-//add_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 4);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 4);
 function ts_woocommerce_single_product_summary(){
     echo '<p style="color:#A03C78"><b>We have the best products!</b></p>';
 }
