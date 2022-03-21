2c6710c6729fc5bc8c5327da95b73f82422d4824
Heli Oza <heli.oza@dcmail.ca>
2022-03-21 15:45:10 -0400

    made changes in footer

--- themes/ts/footer.php
+++ themes/ts/footer.php
@@ -11,20 +11,41 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info grid-container">
+	<footer id="colophon" class="site-footer site-info">
 			<?php 
 				$recipe_args = array(
-
+					'post_type'      => array('ts_recipe'),
+					'post_status'    => 'publish',
+					'posts_per_page' => 3,
 				);
 				$recipe_query = new WP_Query($args);
+				?>
+
+				<div class="grid-container">
+					<div class="grid-x grid-margin-x grid-margin-y">
+
+				
+				<?php
+				
 				if($recipe_query->have_posts()){
 					while($recipe_query->have_posts()){
-						$recipe_query->have_posts();
+						$recipe_query->the_posts();
+				?>
+					<div class="cell small-12 medium-4">
+						<?php 
+						the_post_thumbnail();
+						the_title(); 
+						the_excerpt(); 
+						?>
+					</div>
+					<?php
 					}
 					wp_reset_postdata();
 				}
-			?>
+					?>
+			
+				</div>
+				</div>
 			<?php
 				wp_nav_menu(
 					array(
