721c29ed8908ae88c721c95c8a06a7b6334d5268
Heli Oza <heli.oza@dcmail.ca>
2022-03-27 14:03:35 -0400

    made custom posts types

--- themes/ts/functions.php
+++ themes/ts/functions.php
@@ -184,5 +184,5 @@ require get_template_directory() . '/inc/woocommerce.php';
 
 /*
  * custom posts additions.
-
-require get_template_directory() . '/inc/post-type.php';*/
+*/
+require get_template_directory() . '/inc/post-type.php';
